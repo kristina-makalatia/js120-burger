06c750669858e4f56398717f43584c7827dfc798
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2021-11-12 21:23:40 +0400

    add burger

---
+++ script.js
@@ -0,0 +1,9 @@
+//Events
+
+let navigationBar = document.getElementById('navbar');
+let toggleButton = document.getElementById('toggle-button');
+
+
+toggleButton.addEventListener('click', function() {
+    navigationBar.classList.toggle('active');
+})
